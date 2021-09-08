d199c207708e3ababcacf191cf1c01eab490943f
derek-williams00 <derekOfficial00@gmail.com>
2021-09-08 16:38:31 -0700

    added scoreboard.html

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 # import flask web server
-from flask import Flask
+from flask import Flask, render_template
 
+# create flask app
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/play')
+@app.route('/home')
 def index():
-           return '<h1>Hello World!</h1>'
+    return render_template("home.html")
 
+@app.route('/scoreboard')
+def scoreboard():
+    return render_template("scoreboard.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
