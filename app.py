aabbb7a5e5d10477fc6a5d876fa074e6282ea76c
derek-williams00 <derekOfficial00@gmail.com>
2021-09-10 13:56:21 -0700

    added working 404 page

--- app.py
+++ app.py
@@ -26,17 +26,24 @@ def print_current_games():
         result += " {}".format(game.get_n())
     print(result)
 
+@app.errorhandler(404)
+def error_404(e):
+    return render_template("404.html"), 404
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
+    return render_template("home.html")
+
+@app.route('/new')
+def new_game():
     global games
     game = TicTacToe()
     games.append(game)
     print_current_games()
-    return render_template("home.html", game_n=game.get_n())
+    return render_template("game.html", game_n=game.get_n())
 
-@app.route('/<number>')
+@app.route('/n/<number>')
 def game(number: int): #TODO handle nonexistant games
     game = game_by_n(int(number))
     print_current_games()
@@ -45,7 +52,6 @@ def game(number: int): #TODO handle nonexistant games
 @app.route('/scoreboard')
 def scoreboard():
     return render_template("scoreboard.html")
-
 
 if __name__ == '__main__':
     app.run(debug=True)
